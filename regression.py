fba2c7a2680186836e7faf02c91a4f6451f28aa9
Steve Williams <stevenjw01@gmail.com>
2018-01-07 22:52:44 -0500

    Add files via upload

--- regression.py
+++ regression.py
@@ -12,8 +12,8 @@ style.use('ggplot')
 
 
 
-df = quandl.get('WIKI/TSLA
-                ')
+df = quandl.get('WIKI/TSLA')
+
 df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume',]]
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
